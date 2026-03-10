b343e87838c7defa99671b05b35286e76e888a39
Tom Daniaud <daniaudtom@gmail.com>
2026-03-10 15:13:34 +0100

    init

---
+++ institutmontaigne/frontend/vite.config.js
@@ -0,0 +1,25 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: './',
+  build: {
+    outDir: '../public',
+    emptyOutDir: false,
+  },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://127.0.0.1:8080',
+        changeOrigin: true,
+        rewrite: (p) => `/institutmontaigne${p}`,
+      },
+      '/institutmontaigne': {
+        target: 'http://127.0.0.1:8080',
+        changeOrigin: true,
+      },
+    },
+  },
+})
