d4856aec8a2018f290f6419158a9f1275f958e5a
Bryan <bryan.levy@etu.univ-nantes.fr>
2026-03-11 17:51:15 +0100

    FEAT - Initialisation BDD API

--- actu/api/api.js
+++ actu/api/api.js
@@ -3,6 +3,15 @@
 const app = require( 'express' )();
 const path = require('path');
 
+// BASE DE DONNEES
+
+const sqlite3 = require('sqlite3')
+const { open } = require('sqlite')
+
+app.get('/init', async function ( req, res ) {
+    initialisation();
+    res.json({'Ok':true});
+} );
 // Sample endpoint that sends the partner's name
 app.get('/topic', function ( req, res ) {
     let topic;
@@ -12,6 +21,36 @@ app.get('/topic', function ( req, res ) {
     // Send it as a JSON object
     res.json({'topic':topic});
 } );
+
+
+// BASE DE DONNEES
+
+async function initialisation(){
+    const db = await open({
+    filename: './actu/api/BDD/database.db',
+    driver: sqlite3.Database
+    })
+    db.exec(`
+    CREATE TABLE Utilisateur(
+        id INTEGER PRIMARY KEY,
+        token TEXT
+    ) STRICT
+    `); 
+}
+
+async function initialisation(){
+    const db = await open({
+    filename: './actu/api/BDD/database.db',
+    driver: sqlite3.Database
+    })
+    db.exec(`
+    CREATE TABLE Utilisateur(
+        id INTEGER PRIMARY KEY,
+        token TEXT
+    ) STRICT
+    `); 
+}
+
 
 // Export our API
 module.exports = app;
