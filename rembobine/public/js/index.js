5ec5be69b7516d3d7560d7df92a972bcaddd9196
Matis HADET <matis.hadet@etu.univ-nantes.fr>
2026-03-11 20:04:23 +0100

    test des cases

--- rembobine/public/js/index.js
+++ rembobine/public/js/index.js
@@ -1,17 +1,5 @@
 "use strict";
 
-// Wait for the content to preload and display 1st slide
-// Here we simulate a loading time of one second
-setTimeout(() => { 
-  // fade out the loader "slide"
-  // and send it to the back (z-index = -1)
-  anime({
-    delay: 1000,
-    targets: '#loader',
-    opacity: '0',
-    'z-index' : -1,
-    easing: 'easeOutQuad',
-  });
-  // Init first slide
-  initPageProjet();
-}, 1000);
+
+// Init first slide
+initPageProjet();
--- rembobine/public/js/pageProjet.js
+++ rembobine/public/js/pageProjet.js
@@ -1,13 +1,66 @@
 "use strict";
 
-// async init function (because of the awaits on fetches)
-const initPageProjet = async function(){
+function createButtonBox(boxId = "box1") {
+  const box = document.createElement("div");
+  box.id = boxId;
+  box.className = "box";
 
-  
+  for (let rowIndex = 0; rowIndex < 2; rowIndex += 1) {
+    const row = document.createElement("div");
+    row.className = "row";
+
+    for (let columnIndex = 0; columnIndex < 2; columnIndex += 1) {
+      const buttonNumber = rowIndex * 2 + columnIndex + 1;
+      const button = document.createElement("button");
+      button.id = `button${buttonNumber}`;
+      button.textContent = ` ${buttonNumber} `;
+      row.appendChild(button);
+    }
+
+    box.appendChild(row);
+
+    box.querySelectorAll('button').forEach(option => {
+      option.addEventListener('click', async () => {
+        // Send the user's choice to our API
+
+        const value = option.textContent;
+
+        const box = option.parentElement.parentElement;
+        // Remove all buttons
+        option.parentElement.querySelectorAll('button').forEach(btn => btn.remove());
+
+        // Remove all rows
+        box.querySelectorAll('.row').forEach(row => row.remove());
+
+        // Change box background color
+        box.style.backgroundColor = '#ff2ba3d1'; // Change color as desired
+
+        // Display clicked button text
+        const textDisplay = document.createElement('p');
+        textDisplay.textContent = value;
+        box.appendChild(textDisplay);
+
+        box.parentElement.appendChild(createButtonBox(`box`));
+      });
+    });
+  }
+
+  return box;
+}
+
+// async init function (because of the awaits on fetches)
+const initPageProjet = async function () {
+
+
   // Retrieve the partner's topic from our API
   let response = await fetch('api/topic');
   const data1 = await response.json();
 
   const titre = document.getElementById('titre');
   titre.textContent = `Our topic is "${data1.topic}".`;
+
+  document.getElementById('mapColum1').appendChild(createButtonBox());
+
+
+
 };
