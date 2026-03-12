c7a9caf3e64cf38edf51a7a1780a85f5f32fc9ca
Eliza Medvedev <elizamedvedev@gmail.com>
2026-03-12 12:24:48 +0100

    🔀 fix dernier pb de merge

--- institutmontaigne/developpement/src/App.jsx
+++ institutmontaigne/developpement/src/App.jsx
@@ -1,12 +1,12 @@
 import { useState } from 'react';
 import Hero from './components/Hero.jsx';
-import Header from './components/Header';
+import Header from './components/Header.jsx';
 import Scrollytelling from './components/Scrollytelling';
 import QuestionAccordion from './components/QuestionAccordion';
 import Quiz from './components/Quiz';
+import QuizFin from './components/Quiz_fin';
+import Resultat from './components/Resultat';
 import debateData from './data/debate.jsx';
-import Timeline from './components/infographie/Timeline.jsx';
-import Header from './components/Header.jsx';
 import Footer from './components/Footer';
 
 export default function App() {
@@ -15,7 +15,7 @@ export default function App() {
 
   return (
     <div className="min-h-screen">
-      <Header />
+      <Header meta={debateData.meta} />
       <Hero meta={debateData.meta} />
       <Scrollytelling accroches={debateData.accroches} images={debateData.accrocheImages} />
       <Quiz meta={debateData.meta} onScoreChange={setInitialScore} />
@@ -23,6 +23,8 @@ export default function App() {
         questions={debateData.questions}
         intervenants={debateData.meta.intervenants}
       />
+      <QuizFin onScoreComputed={setFinalScore} />
+      <Resultat initialScore={initialScore} finalScore={finalScore} />
       <Footer />
     </div>
   );
