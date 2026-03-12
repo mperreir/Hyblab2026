ba80b1a4f5c7857f8fde02daf33f524a1cfe53e4
Tom Daniaud <daniaudtom@gmail.com>
2026-03-12 11:42:17 +0100

    masque le header au scroll

--- institutmontaigne/developpement/src/App.jsx
+++ institutmontaigne/developpement/src/App.jsx
@@ -1,17 +1,17 @@
-import Header from './components/Header';
+import Hero from './components/Hero.jsx';
 import Scrollytelling from './components/Scrollytelling';
 import QuestionAccordion from './components/QuestionAccordion';
 import Quiz from './components/Quiz';
 import debateData from './data/debate.jsx';
 import Timeline from './components/infographie/Timeline.jsx';
-import Navbar from './components/Navbar';
+import Header from './components/Header.jsx';
 import Footer from './components/Footer';
 
 export default function App() {
   return (
     <div className="min-h-screen">
-      <Navbar />
-      <Header meta={debateData.meta} />
+      <Header />
+      <Hero meta={debateData.meta} />
       <Scrollytelling accroches={debateData.accroches} images={debateData.accrocheImages} />
       <Quiz meta={debateData.meta} />
       <QuestionAccordion
