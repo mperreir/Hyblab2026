4b4aa6dc05c65a94d108a0cab1044627289106c7
Paul <paul.bodineau@etu.univ-nantes.fr>
2026-03-13 10:32:36 +0100

    Liaison scene ciel et musee ( avant modif)

--- metropolegrandlyon/public/js/zoom.js
+++ metropolegrandlyon/public/js/zoom.js
@@ -8,9 +8,21 @@ const Nuage2_Z  = document.getElementById("nuage2_zoom");
 const Nuage3_Z  = document.getElementById("nuage3_zoom");
 const txt = document.getElementById("TxtInfo");
 
+const calqueVille = document.querySelector('.calque-ville');
+const musee = document.querySelector('.musee');
+const ruines = document.querySelector('.ruines');
+const ecolier = document.querySelector('.ecolier');
+const blocTexteMusee = document.querySelector('.bloc-texte-musee');
+
 //Repères de scroll (en pixels)
 const scrollFinStep1 = 800;
 const scrollFinStep2 = 1200;
+
+const scrollDebutDescente = 1600;
+const scrollFinDescente = 1800;
+const scrollDebutMusee = 2200;
+const scrollDebutRuines = 2600;
+const scrollDebutEcolier = 2800;
 
 //GIF
 const gif1 = document.getElementById("NuageGIF1");
@@ -44,14 +56,11 @@ function clamp(val, min, max) { return Math.min(Math.max(val, min), max); }
 function lerp(a, b, t) { return a + (b - a) * clamp(t, 0, 1); }
 function avancement(scroll, debut, fin) { return clamp((scroll - debut) / (fin - debut), 0, 1); }
 
-function TXT(id,avance){
+function TXT(id){
   fetch('data/data.json')
   .then(response => response.json())
   .then(data => {
-    const opacity = lerp(0, 1, avance);
-
     txt.textContent = data[id]; 
-    txt.style.opacity = opacity;
   });};
 
 window.addEventListener("scroll", () => {
@@ -89,6 +98,10 @@ window.addEventListener("scroll", () => {
     Nuage2_Z.style.opacity = opacity;
     Nuage3_Z.style.opacity = opacity;
 
+    if (scrollActuel < scrollFinStep1){
+            txt.style.opacity = 0;
+        }
+
 
     //Phase2
     const progression  = Math.max(-1, scrollActuel-scrollFinStep1) / (scrollFinStep2-scrollFinStep1);
@@ -113,10 +126,60 @@ window.addEventListener("scroll", () => {
             frameAffichee = indexFrame;
         }
 
-        TXT("ZFE",avancement(scrollActuel-300, scrollFinStep1, scrollFinStep2));
+        TXT("ZFE");
 
-        txt.style.top = "50%";
+        if (scrollActuel > scrollFinStep1 && scrollActuel < scrollFinStep2+50){
+            const opacity = lerp(0, 1, avancement(scrollActuel, scrollFinStep1+10, scrollFinStep2+50));
+            txt.style.opacity = opacity;
+            txt.style.top = "50%";
+        }
+
+        const scaleCiel_inv = lerp(3, 1, avancement(scrollActuel,1500,scrollDebutDescente));
+        ciel.style.transform = `scale(${scaleCiel_inv})`;
+
+        if (scrollActuel > 1500 && scrollActuel < scrollDebutDescente-25){
+            const opa = lerp(1, 0, avancement(scrollActuel,1500,scrollDebutDescente-30));
+            gif1.style.opacity = opa;
+            gif2.style.opacity = opa;
+            gif3.style.opacity = opa;
+            txt.style.opacity = opa;
+        }
     };
+
+    // PHASE 3 : scène musée
+
+
+    if (scrollActuel> scrollDebutDescente){
+
+        const descente = avancement(scrollActuel, scrollDebutDescente, scrollFinDescente);
+
+        // la colline remonte depuis le bas
+        if(calqueVille){
+            calqueVille.style.opacity = 1;
+            calqueVille.style.transform = `translateY(${(80 -descente * 40)}%)  scale(0.13)`;
+        }
+
+        // apparition des éléments
+        if(scrollActuel > scrollDebutMusee  && scrollActuel < scrollDebutRuines){
+            musee.classList.add("visible");
+            const descente2 = avancement(scrollActuel, scrollDebutMusee, scrollDebutMusee+100);
+            musee.style.transform = `translateY(${(-descente2*50)}vh)`;
+        }
+
+        if(scrollActuel > scrollDebutRuines && scrollActuel < scrollDebutEcolier){
+            ruines.classList.add("visible");
+            const descente3 = avancement(scrollActuel, scrollDebutRuines, scrollDebutRuines+100);
+            ruines.style.transform = `translateY(${(-descente3*30)}vh)`;
+        }
+
+        if(scrollActuel > scrollDebutEcolier && scrollActuel < scrollDebutEcolier +200){
+            ecolier.classList.add("visible");
+            //blocTexteMusee.classList.add("visible");
+            const descente4 = avancement(scrollActuel, scrollDebutEcolier, scrollDebutEcolier+100);
+            ecolier.style.transform = `translateY(${(-descente4*30)}vh)`;
+        }
+    }
+
     
     
 }, { passive: true });
